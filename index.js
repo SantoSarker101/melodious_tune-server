60e2c4afde0539cdfe4595a3b0c36176e8cc2222
SantoSarker101 <santosarker404@gmail.com>
2024-06-12 17:52:55 +0600

    Create Admin and Instructor Route

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xrsgd45.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -36,7 +36,7 @@ const verifyJWT = (req, res, next) => {
   const token = authorization.split(' ')[1]
   console.log(token);
   // Token Verify
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+  jwt.verify(token, process.env.ACCESS_SECRET_TOKEN, (err, decoded) => {
     if(err){
       return res.status(401).send({ error: true, message: 'Unauthorized Access' })
     }
@@ -60,16 +60,46 @@ async function run() {
 	 // Generate jwt token
    app.post('/jwt', (req, res) => {
     const email = req.body
-    console.log(email);
+    // console.log(email);
     const token = jwt.sign(email, process.env.ACCESS_SECRET_TOKEN, {expiresIn: '9999999999999d'})
-    console.log(token);
+    // console.log(token);
     res.send({token})
   })
+
+
+// verify Admin
+  const verifyAdmin = async(req, res, next) => {
+    const email = req.decoded.email;
+    const query = { email: email }
+    const user = await usersCollection.findOne(query)
+    if(user?.role !== 'admin'){
+      return res.status(403).send({error: true, message: 'Forbidden Access'});
+    }
+    next()
+  }
+
+
+  // Verify Instructor
+  const verifyInstructor = async(req, res, next) => {
+    const email = req.decoded.email;
+    const query = { email: email }
+    const user = await usersCollection.findOne(query)
+    if(user?.role !== 'Instructor'){
+      return res.status(403).send({error: true, message: 'Forbidden Access'});
+    }
+    next()
+  }
 
 
 
 
   // Users related apis
+  app.get('/users', verifyJWT, async (req, res) => {
+    const result = await usersCollection.find().toArray();
+    res.send(result)
+  })
+
+
   app.put('/users/:email', async(req, res) => {
     const email = req.params.email
     const user = req.body
@@ -79,9 +109,98 @@ async function run() {
       $set: user,
     }
     const result = await usersCollection.updateOne(query, updateDoc, option)
-    console.log(result);
+    // console.log(result);
     res.send(result)
   })
+
+
+  // Create Admin API
+  app.patch('/users/admin/:id', async (req, res) => {
+    const id = req.params.id;
+    const filter = { _id: new ObjectId(id) };
+    const updateDoc = {
+      $set: {
+        role: 'admin'
+      },
+    };
+
+    const result = await usersCollection.updateOne(filter, updateDoc);
+    res.send(result);
+  })
+
+
+
+  // Create Instructor API
+  app.patch('/users/instructor/:id', async (req, res) => {
+    const id = req.params.id;
+    const filter = { _id: new ObjectId(id) };
+    const updateDoc = {
+      $set: {
+        role: 'Instructor'
+      },
+    };
+
+    const result = await usersCollection.updateOne(filter, updateDoc);
+    res.send(result);
+  })
+
+
+
+    // Create Verify Admin API
+    // app.get('/users/admin/:email', verifyJWT, async (req, res) => {
+    //   const email = req.params.email;
+    //   console.log(email);
+
+    //   if(req.decoded.email !== email){
+    //     res.send({ admin: false })
+    //   }
+
+    //   const query = { email: email }
+
+    //   const user = await usersCollection.findOne(query);
+    //   if(user?.role !== 'admin'){
+    //     return res.status(403).send({error: true, message: 'Forbidden Access'});
+    // }
+    //   const result = { admin: user?.role === 'admin' }
+    //   res.send(result);
+    // })
+
+    app.get('/users/admin/:email', verifyJWT, async (req, res) => {
+      const email = req.params.email;
+
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: 'forbidden access' })
+      }
+
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      // let admin = false;
+      if (user) {
+        admin = user?.role === 'admin';
+      }
+      res.send({ admin });
+    })
+
+
+    // Create Verify Instructor API
+    app.get('/users/instructor/:email', verifyJWT, async (req, res) => {
+      const email = req.params.email;
+
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: 'forbidden access' })
+      }
+
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      // let Instructor  = false;
+      if (user) {
+        Instructor  = user?.role === 'Instructor';
+      }
+      res.send({ Instructor  });
+    })
+
+
+
 
 
 
