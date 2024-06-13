e838e20513e2d57c717cf59c2d690e1fdb358700
SantoSarker101 <santosarker404@gmail.com>
2024-06-13 22:56:44 +0600

    Created Delete Method for Delete a class by Instructor

--- index.js
+++ index.js
@@ -232,6 +232,11 @@ async function run() {
 
 
 
+    // Created Class status Approved
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
