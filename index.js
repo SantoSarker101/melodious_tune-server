30ad26e070ac35bee26e1515266d977d715e2c79
SantoSarker101 <santosarker404@gmail.com>
2024-06-15 19:57:54 +0600

    Created Post method for save payment information in mongodb

--- index.js
+++ index.js
@@ -349,7 +349,7 @@ async function run() {
 
 
     // Payment Related API
-    app.post('/payments', async (req, res) => {
+    app.post('/payments', verifyJWT, async (req, res) => {
       const payment = req.body;
       const result = await paymentCollection.insertOne(payment)
       res.send(result)
